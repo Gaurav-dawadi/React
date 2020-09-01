10a2d219ab4f3cefce737b3f61d0de52f4bff15d
Gaurab <gaurabdawadi75@gmail.com>
2020-09-01 17:49:48 +0545

    feat: create component and get location

--- seasoned/src/App.js
+++ seasoned/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    navigator.geolocation.getCurrentPosition(
+        (success) => console.log(success),
+         (error) => console.log(error)
+    );
+
+    return(
+        <div>
+            <h1>Hey There</h1>
+
+        </div>
+    )
 }
 
-export default App;
+export default App
