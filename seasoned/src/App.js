bf54f0769c54255beec169b95f3667277849eabc
Gaurab <gaurabdawadi75@gmail.com>
2020-09-01 18:07:53 +0545

    refactor: change functional components to class components

--- seasoned/src/App.js
+++ seasoned/src/App.js
@@ -1,18 +1,43 @@
 import React from 'react'
 
-const App = () => {
+// const App = () => {
 
-    navigator.geolocation.getCurrentPosition(
-        (success) => console.log(success),
-         (error) => console.log(error)
-    );
+//     navigator.geolocation.getCurrentPosition(
+//         (success) => console.log(success),
+//         (error) => console.log(error)
+//     );
 
-    return(
-        <div>
-            <h1>Hey There</h1>
+//     return(
+//         <div>
+//             <h1>Hey There</h1>
+//         </div>
+//     )
+// }
 
-        </div>
-    )
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            latitude: null,
+        }
+    }
+
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition(
+            (success) => {
+                this.setState({
+                    latitude: success.coords.latitude
+                })
+            },
+             (error) => console.log(error)
+        );
+    }
+
+    render(){
+        return(
+            <div>Latitude: {this.state.latitude}</div>
+        )
+    }
 }
 
 export default App
