19cc7be90e636461926b8bf03be16a8db085a919
Gaurab <gaurabdawadi75@gmail.com>
2020-09-02 19:04:33 +0545

    feat: add uncontrolled form/elements

--- search-and-display-pics/src/components/SearchBar.js
+++ search-and-display-pics/src/components/SearchBar.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
 class SearchBar extends React.Component{
+
+    onInputChange(event){
+        console.log(event.target.value);
+    }
+
     render(){
         return(
             <div className='ui segment'>
                 <form className='ui form'>
                     <div class="field">
-                        <input type="text" placeholder="Search......" />
+                        {/* this.onInputChange() is incorrect because we are not calling
+                            function here instead using its reference only. If we use parenthesis
+                            then every time component re-render then function will be called which
+                            we donot want. We want to call it in certain time only.*/}
+                        <input type="text" onChange={this.onInputChange} placeholder="Search......" />
                     </div>
                 </form>
             </div>
