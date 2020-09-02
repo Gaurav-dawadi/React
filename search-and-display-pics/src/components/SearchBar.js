b79db90e283640b39489f132e80f35e7f888b64b
Gaurab <gaurabdawadi75@gmail.com>
2020-09-02 19:09:35 +0545

    refactor: change uncontrolled to controlled elements

--- search-and-display-pics/src/components/SearchBar.js
+++ search-and-display-pics/src/components/SearchBar.js
@@ -2,20 +2,18 @@ import React from 'react'
 
 class SearchBar extends React.Component{
 
-    onInputChange(event){
-        console.log(event.target.value);
-    }
+    state = {term: ''};
 
     render(){
         return(
             <div className='ui segment'>
                 <form className='ui form'>
-                    <div class="field">
-                        {/* this.onInputChange() is incorrect because we are not calling
-                            function here instead using its reference only. If we use parenthesis
-                            then every time component re-render then function will be called which
-                            we donot want. We want to call it in certain time only.*/}
-                        <input type="text" onChange={this.onInputChange} placeholder="Search......" />
+                    <div class="field">           
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={e => this.setState({term: e.target.value})}
+                            placeholder="Search......" />
                     </div>
                 </form>
             </div>
