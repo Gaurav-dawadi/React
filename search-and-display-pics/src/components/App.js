f28d7890f7664600a728533192f5f838fb07b29e
Gaurab <gaurabdawadi75@gmail.com>
2020-09-04 13:48:07 +0545

    feat: handle requests with async await and set state

--- search-and-display-pics/src/components/App.js
+++ search-and-display-pics/src/components/App.js
@@ -2,10 +2,22 @@ import React from 'react'
 import axios from 'axios'
 import SearchBar from './SearchBar'
 
+
 class App extends React.Component {
 
-    onSearchSubmit(term){      
-        console.log(term);
+    state = {images: []}
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers:{
+                Authorization:
+                    'Client-ID 8cN1LMBqjX7rLfnYxdHS-OlzwqdaU_lGBoPjhiISJ7E'
+            }
+        });
+        
+        this.setState({images: response.data.results});
+        console.log(response.data.results);
     }
 
     render(){
@@ -13,6 +25,7 @@ class App extends React.Component {
             <div className='ui container' style={{marginTop: '20px'}}>
                 <h3>Search Image</h3>
                 <SearchBar whenSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         ) 
     }
