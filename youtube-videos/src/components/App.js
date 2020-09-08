baf4d616331798ee8998b1e3fbd58f9392414b21
Gaurab <gaurabdawadi75@gmail.com>
2020-09-08 13:17:40 +0545

    feat: add props in App component and use it in Search component

--- youtube-videos/src/components/App.js
+++ youtube-videos/src/components/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import SearchBar from './searchbar'
+import YouTube from '../API/youtube'
 
 class App extends React.Component{
+
+    onTermSubmit = (term) => {
+        YouTube.get('/search', {
+            params:{
+                q: term
+            }
+        })
+    }
+
     render(){
         return(
             <div className='ui container'>
                 <h1>Hey There</h1>
                 
-                <SearchBar />
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
 
             </div>
         )
